b75c76c52842d7f7eb7d9f37129a654b2c920a6e
Balamurugan C <mikeybala123@gmail.com>
2026-06-14 20:49:18 +0530

    update

---
+++ JS/day1/script.js
@@ -0,0 +1,245 @@
+// Varible names using Var
+var studentName;
+console.log(studentName);
+
+var studentAge;
+console.log(studentAge);
+
+var courseName;
+console.log(courseName);
+
+var collegeName;
+console.log(collegeName);
+
+var cityName;
+console.log(cityName);
+
+var phoneNumber;
+console.log(phoneNumber);
+
+var emailAddress;
+console.log(emailAddress);
+
+var teacherName;
+console.log(teacherName);
+
+var subjectName;
+console.log(subjectName);
+
+var projectTitle;
+console.log(projectTitle);
+
+var bookName;
+console.log(bookName);
+
+var laptopBrand;
+console.log(laptopBrand);
+
+var mobileBrand;
+console.log(mobileBrand);
+
+var companyName;
+console.log(companyName);
+
+var departmentName;
+console.log(departmentName);
+
+var countryName;
+console.log(countryName);
+
+var stateName;
+console.log(stateName);
+
+var favoriteColor;
+console.log(favoriteColor);
+
+var busNumber;
+console.log(busNumber);
+
+var examScore;
+console.log(examScore);
+
+
+// declaration using Let
+
+let employeeName;
+console.log(employeeName);
+
+let employeeId;
+console.log(employeeId);
+
+let officeName;
+console.log(officeName);
+
+let managerName;
+console.log(managerName);
+
+let teamName;
+console.log(teamName);
+
+let taskName;
+console.log(taskName);
+
+let meetingRoom;
+console.log(meetingRoom);
+
+let salaryAmount;
+console.log(salaryAmount);
+
+let joiningYear;
+console.log(joiningYear);
+
+let workLocation;
+console.log(workLocation);
+
+let customerName;
+console.log(customerName);
+
+let orderNumber;
+console.log(orderNumber);
+
+let productName;
+console.log(productName);
+
+let productPrice;
+console.log(productPrice);
+
+let stockCount;
+console.log(stockCount);
+
+let supplierName;
+console.log(supplierName);
+
+let paymentStatus;
+console.log(paymentStatus);
+
+let deliveryDate;
+console.log(deliveryDate);
+
+let vehicleNumber;
+console.log(vehicleNumber);
+
+let driverName;
+console.log(driverName);
+
+let routeName;
+console.log(routeName);
+
+let hospitalName;
+console.log(hospitalName);
+
+let doctorName;
+console.log(doctorName);
+
+let patientName;
+console.log(patientName);
+
+let roomNumber;
+console.log(roomNumber);
+
+let ticketNumber;
+console.log(ticketNumber);
+
+let movieName;
+console.log(movieName);
+
+let theaterName;
+console.log(theaterName);
+
+let seatNumber;
+console.log(seatNumber);
+
+let eventName;
+console.log(eventName);
+
+// Varible names using const
+
+const libraryCardNumber = 1001;
+console.log(libraryCardNumber);
+
+const trainTicketNumber = 2456;
+console.log(trainTicketNumber);
+
+const flightCode = "AI302";
+console.log(flightCode);
+
+const passportNumber = "N1234567";
+console.log(passportNumber);
+
+const hotelRoomNumber = 205;
+console.log(hotelRoomNumber);
+
+const restaurantName = "Spice Garden";
+console.log(restaurantName);
+
+const foodOrderId = 7890;
+console.log(foodOrderId);
+
+const deliveryPartnerName = "Suresh";
+console.log(deliveryPartnerName);
+
+const movieTicketPrice = 250;
+console.log(movieTicketPrice);
+
+const cricketTeamName = "Chennai Kings";
+console.log(cricketTeamName);
+
+const playerJerseyNumber = 7;
+console.log(playerJerseyNumber);
+
+const gymMembershipId = 5678;
+console.log(gymMembershipId);
+
+const workoutDuration = 60;
+console.log(workoutDuration);
+
+const electricityBillAmount = 1850;
+console.log(electricityBillAmount);
+
+const waterTankCapacity = 1000;
+console.log(waterTankCapacity);
+
+const internetPlanName = "Premium Fiber";
+console.log(internetPlanName);
+
+const wifiPassword = "home1234";
+console.log(wifiPassword);
+
+const bankBranchName = "Ambattur";
+console.log(bankBranchName);
+
+const transactionReference = "TXN45892";
+console.log(transactionReference);
+
+const insurancePolicyNumber = 987654;
+console.log(insurancePolicyNumber);
+
+const vehicleRegistrationNumber = "TN01AB1234";
+console.log(vehicleRegistrationNumber);
+
+const fuelPricePerLiter = 103;
+console.log(fuelPricePerLiter);
+
+const parkingSlotNumber = 45;
+console.log(parkingSlotNumber);
+
+const medicineName = "Paracetamol";
+console.log(medicineName);
+
+const bloodGroupType = "O+";
+console.log(bloodGroupType);
+
+const hospitalWardNumber = 12;
+console.log(hospitalWardNumber);
+
+const eventVenueName = "Trade Center";
+console.log(eventVenueName);
+
+const seminarTopicName = "Artificial Intelligence";
+console.log(seminarTopicName);
+
+const certificateIssueDate = "15-06-2026";
+console.log(certificateIssueDate);
+
+const shoppingCartTotal = 3499;
+console.log(shoppingCartTotal);
